b5480f8468d6cae90031c68a011ea33b03ae2f36
usman <usmanganishaik99@gmail.com>
2023-12-10 22:07:35 +0530

    variable

---
+++ 05-count/count.tf
@@ -0,0 +1,15 @@
+variable "components" {
+
+  default = ["frontend","catalogue"]
+}
+
+
+resource "aws_instance" "services" {
+  count = length(var.components)
+  ami           = "ami-023c11a32b0207432"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
