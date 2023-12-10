27dcd1b3e733c521a25b2f8733761a900eff7822
usman <usmanganishaik99@gmail.com>
2023-12-10 20:41:24 +0530

    variable

--- 04-data_sources/datasources.tf
+++ 04-data_sources/datasources.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "RHEL-9.3.0_HVM-20231101-x86_64-5-Hourly2-GP2"
-  owners           = ["309956199498	"]
+  owners           = ["309956199498"]
 }
 output "aws_ami_data" {
   value = data.aws_ami.ami
