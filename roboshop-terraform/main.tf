6c9d8a8a81e1700177660159802e7448ad17106b
usman <usmanganishaik99@gmail.com>
2023-12-14 10:21:33 +0530

    variable

--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -1,10 +1,9 @@
-terraform {
-  backend "s3" {
-  }
-}
+module "test" {
+  source = "./module"
+  for_each = var.components
+  instance_type =each.value["instance_type"]
+  vpc_security_group_ids =var.vpc_security_group_ids
+  zone_id=var.zone_id
+  name=each.value["name"]
 
-variable "test" {}
-
-output "run_state_file" {
-  value = var.test
 }
