456325610c2c1e104a5ecd2d3d74d5bc7c5067af
usman <usmanganishaik99@gmail.com>
2023-12-15 20:42:08 +0530

    variable

--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -2,7 +2,7 @@ module "components" {
   source = "git::https://github.com/shaikusmandevops/tf-module-roboshop.git"
   for_each = var.components
   instance_type =each.value["instance_type"]
-  security_groups =var.vpc_security_group_ids
+  security_groups =var.security_groups
   zone_id=var.zone_id
   name=each.value["name"]
 }
