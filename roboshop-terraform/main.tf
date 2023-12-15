769f967c474d93f8650b095df8bc11d622ffe1a2
usman <usmanganishaik99@gmail.com>
2023-12-15 18:27:13 +0530

    variable

--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -1,5 +1,5 @@
 module "test" {
-  source = "git::https://github.com/shaikusmandevops/tf-module-basics.git"
+  source = "git::https://github.com/shaikusmandevops/aws-terraform/tree/b21afa870a1e4c394c262882f784e0034e74afcb/tf-module-basics"
   for_each = var.components
   instance_type =each.value["instance_type"]
   vpc_security_group_ids =var.vpc_security_group_ids
